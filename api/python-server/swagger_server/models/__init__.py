a8ec338574fdc8532416054d5d0ea38da466ade2
fkain <fk@ong.at>
2020-09-30 23:28:28 +0200

    api server and client code (generated)

---
+++ api/python-server/swagger_server/models/__init__.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+# flake8: noqa
+from __future__ import absolute_import
+# import models into model package
+from swagger_server.models.cube_state import CubeState
+from swagger_server.models.handler import Handler
+from swagger_server.models.http_request import HttpRequest
+from swagger_server.models.side_id import SideId
+from swagger_server.models.system_config import SystemConfig
+from swagger_server.models.system_state import SystemState
