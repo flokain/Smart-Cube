900b90c376b6b9871fb1b96b25f045779145f516
fkain <fk@ong.at>
2020-10-20 17:30:36 +0200

    tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -2,19 +2,33 @@ import requests
 
 host = "10.42.0.70"
 
-payload = "{\"value\": true}"
-headers = {
-  'Content-Type': 'application/json'
-}
-response = requests.request("PUT", f"http://{host}/api/gpio/2", headers=headers, data = payload)
+payload = '{"value": true}'
+headers = {"Content-Type": "application/json"}
+
 
 def test_gpio_set_pin():
-    response = requests.request("PUT", f"http://{host}/api/gpio/2", headers=headers, data = payload)
+    response = requests.request(
+        "PUT", f"http://{host}/api/v1/gpio/2", headers=headers, data=payload
+    )
     assert response.ok
-    assert response.json() ==  {'message': 'changed', 'value': 1}
+    assert response.json() == {"message": "changed", "value": 1}
 
-def test_gpio_set_pin():
-    response = requests.request("PUT", f"http://{host}/api/gpio/2", headers=headers, data = payload)
+
+def test_gpio_get_side_handler():
+    response = requests.request(
+        "GET", f"http://{host}/api/v1/handler/side/1", headers=headers
+    )
     assert response.ok
-    assert response.json() ==  {'message': 'changed', 'value': 1}
 
+    assert response.json() == {
+        "request": {
+            "uri": "http://my-remote-service.com/webhook-to-trigger",
+            "method": "POST",
+            "headers": {
+                "Authorization": "Basic NzQwYzefIzZjhjYTEwMadadasd",
+                "accept": "application/json",
+            },
+            "payload": "sample body as string",
+        },
+        "expectedResponse": 201,
+    }
