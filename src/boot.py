5901fbd80489189eab1b834753a2fef4732fb965
fkain <fk@ong.at>
2020-11-13 07:51:14 +0100

    powe opti: change leep mode

--- src/boot.py
+++ src/boot.py
@@ -1,4 +1,5 @@
 import network
-
+import esp
+esp.sleep_type(esp.SLEEP_LIGHT) # safe power at the cost of lower reaction time (3ms to wake up)
 network.WLAN(network.STA_IF).active(False)  # WiFi station interface
 network.WLAN(network.AP_IF).active(False)  # access-point interface
