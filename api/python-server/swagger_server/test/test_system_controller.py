c8ce8f38e9c0ef3904a0b887baa6bdc1d845d1f3
fkain <fk@ong.at>
2020-11-21 23:52:14 +0100

    new api version

--- api/python-server/swagger_server/test/test_system_controller.py
+++ api/python-server/swagger_server/test/test_system_controller.py
@@ -6,31 +6,35 @@ from flask import json
 from six import BytesIO
 
 from swagger_server.models.system_config import SystemConfig  # noqa: E501
+from swagger_server.models.wifi import Wifi  # noqa: E501
 from swagger_server.test import BaseTestCase
 
 
 class TestSystemController(BaseTestCase):
     """SystemController integration test stubs"""
 
-    def test_system_config_get(self):
-        """Test case for system_config_get
+    def test_system_config_wifi_get(self):
+        """Test case for system_config_wifi_get
 
-        get the configuration
+        get configured wifi(s)
         """
         response = self.client.open(
-            '/System/config',
+            '/System/config/wifi',
             method='GET')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
-    def test_system_config_post(self):
-        """Test case for system_config_post
+    def test_system_config_wifi_post(self):
+        """Test case for system_config_wifi_post
 
-        set the configuration
+        return configured wifi
         """
+        body = Wifi()
         response = self.client.open(
-            '/System/config',
-            method='POST')
+            '/System/config/wifi',
+            method='POST',
+            data=json.dumps(body),
+            content_type='application/json')
         self.assert200(response,
                        'Response body is : ' + response.data.decode('utf-8'))
 
