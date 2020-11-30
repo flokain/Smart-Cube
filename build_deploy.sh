049912a2c4a7d41b986b72df399414a3fb4b400f
fkain <fk@ong.at>
2020-11-30 12:41:05 +0100

    update scripts to use env

--- build_deploy.sh
+++ build_deploy.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
+device=${SMART_CUBE_PORT:='/dev/ttyUSB0' }
 
 docker run --rm -v $HOME:$HOME -u 1000 -w $PWD/misc/micropython/ports/esp8266/ larsks/esp-open-sdk make
-esptool.py --port /dev/ttyUSB0 --baud 460800 write_flash --flash_size=detect 0 $PWD/misc/micropython/ports/esp8266/build-GENERIC/firmware-combined.bin
-
+esptool.py --port $device --baud 460800 write_flash --flash_size=detect 0 $PWD/misc/micropython/ports/esp8266/build-GENERIC/firmware-combined.bin
