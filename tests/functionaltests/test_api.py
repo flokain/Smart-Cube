1ba74176b739fcec174e24dc2beadcb72793d005
fkain <fk@ong.at>
2021-02-11 22:55:57 +0100

    added tests

--- tests/functionaltests/test_api.py
+++ tests/functionaltests/test_api.py
@@ -1,11 +1,7 @@
 import requests
 import unittest
-import pprint
-import sys
-pprint.pprint(sys.path)
 
-host = "10.0.0.20"
-
+host = "192.168.0.139"
 headers = {"Content-Type": "application/json"}
 
 
@@ -17,66 +13,120 @@ def test_gpio_set_pin():
     assert response.json() == {"message": "changed", "value": 1}
 
 
-class SideHandlerTestCase(unittest.TestCase):
+class Resourcetesting():
 
     def setUp(self):
-        self.payload = {
-            "request": {
-                "uri": "https://www.toggl.com/api/v8/time_entries/start",
-                "method": "POST",
-                "headers": {
-                    "Authorization": "Basic NzQwYzIzZjhjYTEwMzQwMDY3Mjk5NTllMzNjYTg5ODY6YXBpX3Rva2Vu",
-                    "Content-Type": "application/json"
-                },
-                "payload": "{ \"time_entry\":\n  { \"description\": \"triggered by smartcube\",\n \"tags\": [\"triggered by smartcube\"],\n    \"pid\":164777840,\n    \"created_with\":\"curl\"\n    }\n}"
-            },
-            "expectedResponse": 200
-        }
+        self.payload = None
+        self.response = None
+        self.path = None
 
-        self.response = {
-            "id": 23,
-            "request": {
-                "uri": "https://www.toggl.com/api/v8/time_entries/start",
-                "method": "POST",
-                "headers": {
-                    "Authorization": "Basic NzQwYzIzZjhjYTEwMzQwMDY3Mjk5NTllMzNjYTg5ODY6YXBpX3Rva2Vu",
-                    "Content-Type": "application/json"
-                },
-                "payload": "{ \"time_entry\":\n  { \"description\": \"triggered by smartcube\",\n \"tags\": [\"triggered by smartcube\"],\n    \"pid\":164777840,\n    \"created_with\":\"curl\"\n    }\n}"
-            },
-            "expectedResponse": 200
-        }
-
-    def test_get_side_handler(self):
+    def test_get_resource(self):
         response = requests.request(
-            "GET", f"http://{host}/api/v1/handler/side/1", headers=headers
+            "GET", f"{self.path}/1", headers=headers
         )
         assert response.ok
 
-    def test_post_side_handler(self):
+    def test_get_resources(self):
         response = requests.request(
-            "POST", f"http://{host}/api/v1/handler/side", headers=headers, json=self.payload
+            "GET", f"{self.path}", headers=headers
+        )
+        assert response.ok
+
+    def test_post_resource(self):
+        response = requests.request(
+            "POST", f"{self.path}", headers=headers, json=self.payload
         )
 
         assert response.ok
 
-    def test_put_side_handler(self):
+    def test_put_resource(self):
         response = requests.request(
-            "PUT", f"http://{host}/api/v1/handler/side/23", headers=headers, json=self.payload
+            "PUT", f"{self.path}/23", headers=headers, json=self.payload
         )
 
         assert response.ok
         assert response.json() == self.response
 
-    def test_post_and_get_side_handler(self):
+    def test_post_and_get_and_delete_resource(self):
         post_response = requests.request(
-            "POST", f"http://{host}/api/v1/handler/side", headers=headers, json=self.payload
+            "POST", f"{self.path}", headers=headers, json=self.payload
         )
-        id = post_response.json()["id"]
+        id = int(post_response.json()["id"])
         assert post_response.ok
 
         get_response = requests.request(
-            "GET", f"http://{host}/api/v1/handler/side/{id}", headers=headers
+            "GET", f"{self.path}/{id}", headers=headers
         )
         assert get_response.ok
         assert get_response.content == post_response.content
+
+        # TODO: #18 with postman this works. there must be a problem with the headers..
+        delete_response = requests.request(
+            "DELETE", f"{self.path}/{id}", headers=headers
+        )
+        assert delete_response.ok
+        get_response = requests.request(
+            "GET", f"{self.path}/{id}", headers=headers
+        )
+        assert get_response.status_code == 404
+
+    def test_post_and_get_resource(self):
+        post_response = requests.request(
+            "POST", f"{self.path}", headers=headers, json=self.payload
+        )
+        id = int(post_response.json()["id"])
+        assert post_response.ok
+
+        get_response = requests.request(
+            "GET", f"{self.path}/{id}", headers=headers
+        )
+        assert get_response.ok
+        assert get_response.content == post_response.content
+
+        # TODO: #18 with postman this works. there must be a problem with the headers..
+
+
+class SideHandlerTestCase(Resourcetesting, unittest.TestCase):
+
+    def setUp(self):
+        self.payload = {
+            "event-id": "side-1",
+            "request": {
+                "uri": "https://www.toggl.com/api/v8/time_entries/start",
+                "method": "POST",
+                "headers": {
+                    "Authorization": "Basic NzQwYzIzZjhjYTEwMzQwMDY3Mjk5NTllMzNjYTg5ODY6YXBpX3Rva2Vu",
+                    "Content-Type": "application/json"
+                },
+                "payload": "{ \"time_entry\":\n  { \"description\": \"triggered by smartcube\",\n \"tags\": [\"triggered by smartcube\"],\n    \"pid\":164777840,\n    \"created_with\":\"curl\"\n    }\n}"
+            },
+            "expected-response-code": 200
+        }
+
+        self.response = dict(self.payload)
+        self.response["id"] = 23
+        self.path = f"http://{host}/api/v1/handlers"
+
+
+class WifiTestCase(Resourcetesting, unittest.TestCase):
+    def setUp(self):
+        self.payload = {
+            "ssid": "test",
+            "password": "123"
+        }
+        self.path = f"http://{host}/api/v1/system/config/wifis"
+
+        self.response = dict(self.payload)
+        self.response["id"] = 23
+
+
+class UserTestCase(Resourcetesting, unittest.TestCase):
+    def setUp(self):
+        self.payload = {
+            "username": "test",
+            "password": "123"
+        }
+        self.response = dict(self.payload)
+        self.response["id"] = 23
+
+        self.path = f"http://{host}/api/v1/users"
