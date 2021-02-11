ba6a5b480e85244906fdbdedbca60113d005d8f6
fkain <fk@ong.at>
2021-02-11 22:19:33 +0100

    update scripts

--- build_deploy.sh
+++ build_deploy.sh
@@ -17,8 +17,9 @@ sed -i 's/    irom0_0_seg :  org = 0x40209000, len = .*/    irom0_0_seg :  org =
 micropython -m upip install -p $PWD/misc/micropython/ports/esp8266/modules/ -r requirements.txt
 cp -r $PWD/misc/micropython-wifimanager/wifi_manager $PWD/misc/micropython/ports/esp8266/modules/
 cp -r $PWD/misc/tinyweb/tinyweb $PWD/misc/micropython/ports/esp8266/modules/
+rm -r $PWD/misc/micropython/ports/esp8266/modules/smartcube
 cp -r $PWD/src/smartcube $PWD/misc/micropython/ports/esp8266/modules/
 
 docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/mpy-cross/ larsks/esp-open-sdk make
-docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/ports/esp8266/ larsks/esp-open-sdk make AXTLS_DEFS_EXTRA="-Dabort=abort_ -DRT_MAX_PLAIN_LENGTH=1024 -DRT_EXTRA=8192" build
+docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/ports/esp8266/ larsks/esp-open-sdk make AXTLS_DEFS_EXTRA="-Dabort=abort_ -DRT_MAX_PLAIN_LENGTH=1024 -DRT_EXTRA=8192"
 docker run --rm --device=$device -v $PWD/misc/micropython:/micropython -u 1000:$device_group  -w /micropython/ports/esp8266/ larsks/esp-open-sdk make PORT=$device BAUD=460800 deploy
--- clean_build_deploy.sh
+++ clean_build_deploy.sh
@@ -19,8 +19,8 @@ cp -r $PWD/src/smartcube $PWD/misc/micropython/ports/esp8266/modules/
 
 
 docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/mpy-cross/ larsks/esp-open-sdk make
-docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/ports/esp8266/ larsks/esp-open-sdk make clean
-docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/ports/esp8266/ larsks/esp-open-sdk make
+sudo rm -rf $PWD/misc/micropython/ports/esp8266/build-GENERIC
+docker run --rm -v $PWD/misc/micropython:/micropython -u 1000 -w /micropython/ports/esp8266/ larsks/esp-open-sdk make AXTLS_DEFS_EXTRA="-Dabort=abort_ -DRT_MAX_PLAIN_LENGTH=1024 -DRT_EXTRA=8192"
 docker run --rm --device=$device -v $PWD/misc/micropython:/micropython -u 1000:$device_group  -w /micropython/ports/esp8266/ larsks/esp-open-sdk make PORT=$device BAUD=460800 erase
 docker run --rm --device=$device -v $PWD/misc/micropython:/micropython -u 1000:$device_group  -w /micropython/ports/esp8266/ larsks/esp-open-sdk make PORT=$device BAUD=460800 deploy
 
